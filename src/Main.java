1e44791280e5d48a56bfdc995f4b7ec195e73346
vladnlv <efima2506@mail.ru>
2022-10-24 10:06:57 +0200

    created sum function for int

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
+
+    public int sumInt(int a, int b){
+        return a + b;
+    }
 }
